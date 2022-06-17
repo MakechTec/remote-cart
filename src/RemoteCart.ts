7dda4c48fdf8383d944e23943608b9e07274a7a3
Angel Dominguez <angeldomp49@gmail.com>
2022-06-16 19:37:45 -0500

    testing

--- src/RemoteCart.ts
+++ src/RemoteCart.ts
@@ -4,13 +4,29 @@ import {CartItem} from "@makechtec/cart-guidelines";
 
 export class RemoteCart implements Cart{
 
-    list(onSuccess: Function, makeUrl: Function = defaultUrls.list): void {
+    host: string;
+    defaultUrls = {
+        list: () => this.host+"/cart/list",
+        clean: () => this.host+"/cart/clean",
+        add: (item: CartItem) => this.host+`/cart/add/${item.id}`,
+        remove: (item: CartItem) => this.host+`/cart/remove/${item.id}`,
+        find: (id: number) => this.host+`/cart/find/${id}`,
+        total: (attributeName: string) => this.host+`/cart/total/${attributeName}`,
+        numberOfItems: () => this.host+"/cart/numberOfItems",
+    };
+
+    constructor(host: string = "http://localhost:8000", defaultUrls: any = {}){
+        this.host = host;
+        this.defaultUrls = {...this.defaultUrls, ...defaultUrls};
+    }
+
+    list(onSuccess: Function, makeUrl: Function = this.defaultUrls.list): void {
         this.simpleCall(onSuccess, makeUrl());
     }
-    clean(onSuccess: Function, makeUrl: Function = defaultUrls.clean): void {
+    clean(onSuccess: Function, makeUrl: Function = this.defaultUrls.clean): void {
         this.removeCall(onSuccess, makeUrl());
     }
-    add(item: CartItem, onSuccess: Function, makeUrl = defaultUrls.add): void {
+    add(item: CartItem, onSuccess: Function, makeUrl = this.defaultUrls.add): void {
         axios.post(makeUrl(item), {
             attributes: JSON.stringify(item.attributes),
         }).then(response => {
@@ -19,16 +35,16 @@ export class RemoteCart implements Cart{
             this.handleError(error);
         });
     }
-    remove(item: CartItem, onSuccess: Function, makeUrl: Function = defaultUrls.remove): void {
+    remove(item: CartItem, onSuccess: Function, makeUrl: Function = this.defaultUrls.remove): void {
         this.removeCall(onSuccess, makeUrl(item));
     }
-    find(id: number, onSuccess: Function, makeUrl: Function = defaultUrls.find): void {
+    find(id: number, onSuccess: Function, makeUrl: Function = this.defaultUrls.find): void {
         this.simpleCall(onSuccess, makeUrl(id));
     }
-    total(attributeName: string, onSuccess: Function, makeUrl: Function = defaultUrls.total): void {
+    total(attributeName: string, onSuccess: Function, makeUrl: Function = this.defaultUrls.total): void {
         this.simpleCall(onSuccess, makeUrl(attributeName));
     }
-    numberOfItems(onSuccess: Function, makeUrl: Function = defaultUrls.numberOfItems): void {
+    numberOfItems(onSuccess: Function, makeUrl: Function = this.defaultUrls.numberOfItems): void {
         this.simpleCall(onSuccess, makeUrl());
     }
 
@@ -38,7 +54,8 @@ export class RemoteCart implements Cart{
 
     simpleCall(onSuccess: Function, url: string){
         axios.get(url).then(response => {
-            onSuccess(response.data);
+            let data = response.data;
+            onSuccess(data);
         }).catch(error => {
             this.handleError(error);
         });
@@ -46,7 +63,8 @@ export class RemoteCart implements Cart{
 
     removeCall(onSuccess: Function, url: string){
         axios.delete(url).then(response => {
-            onSuccess(response.data);
+            let data = response.data;
+            onSuccess(data);
         }).catch(error => {
             this.handleError(error);
         });
@@ -54,12 +72,3 @@ export class RemoteCart implements Cart{
     
 }
 
-export const defaultUrls = {
-    list: () => "/cart/list",
-    clean: () => "/cart/clean",
-    add: (item: CartItem) => `/cart/add/${item.id}`,
-    remove: (item: CartItem) => `/cart/remove/${item.id}`,
-    find: (id: number) => `/cart/find/${id}`,
-    total: (attributeName: string) => `/cart/total/${attributeName}`,
-    numberOfItems: () => "/cart/numberOfItems",
-}
