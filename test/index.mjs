aea3966e76c1d01ce5b6a29e8c8bc360c7f0ce07
Angel Dominguez <angeldomp49@gmail.com>
2022-06-17 14:20:03 -0500

    using terser

---
+++ test/index.mjs
@@ -0,0 +1,5 @@
+import * as all from "../prebuild/RemoteCart.js";
+import * as all2 from "../index.js";
+
+console.log(all);
+console.log(all2);
---
+++ test/test.mjs
@@ -0,0 +1,3 @@
+
+    let c = new RemoteCart();
+    console.log(c);
